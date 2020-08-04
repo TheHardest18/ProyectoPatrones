9b0d86d1f5ae2968ff9fb7a67559c59a2d6268b1
IsraelPeniel <penielpmd@users.noreply.github.com>
2020-08-03 22:32:48 -0400

    este soy yo

--- ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
+++ ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
@@ -29,14 +29,26 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Write));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 126);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "este soy yo";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Write
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 429);
+            this.ClientSize = new System.Drawing.Size(456, 528);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Write";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Write";
@@ -45,5 +57,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
