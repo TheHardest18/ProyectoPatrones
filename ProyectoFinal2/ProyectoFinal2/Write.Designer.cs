ff4ac7c9a90e65902dda9fc31455d9bf8a0a2128
TheHardest18 <isias1626@gmail.com>
2020-08-03 22:36:22 -0400

    jjjj

--- ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
+++ ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
@@ -34,21 +34,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 366);
+            this.button1.Location = new System.Drawing.Point(76, 297);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(224, 126);
+            this.button1.Size = new System.Drawing.Size(168, 102);
             this.button1.TabIndex = 0;
             this.button1.Text = "este soy yo";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Write
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(456, 528);
+            this.ClientSize = new System.Drawing.Size(334, 431);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Write";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Write";
