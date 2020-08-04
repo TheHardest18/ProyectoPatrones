9e3624c956ab4b1ff5d0133230e688bdec17d762
TheHardest18 <isias1626@gmail.com>
2020-08-03 22:45:05 -0400

    prueba

--- ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
+++ ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
@@ -29,24 +29,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Write));
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(130, 83);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Write
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(342, 429);
-            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Write";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -56,7 +45,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
