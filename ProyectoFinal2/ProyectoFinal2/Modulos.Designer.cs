62e238a0f1cee8f7a4fcd0f3d5fb44d0e318829c
TheHardest18 <isias1626@gmail.com>
2020-08-03 22:52:13 -0400

    hhh

--- ProyectoFinal2/ProyectoFinal2/Modulos.Designer.cs
+++ ProyectoFinal2/ProyectoFinal2/Modulos.Designer.cs
@@ -31,13 +31,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.AutoEllipsis = true;
             this.button1.Location = new System.Drawing.Point(240, 116);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(176, 61);
             this.button1.TabIndex = 0;
@@ -58,18 +59,28 @@
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(68, 48);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(176, 58);
             this.button3.TabIndex = 2;
             this.button3.Text = "con klk y klk";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(110, 216);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 66);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "y klk";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Modulos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(427, 429);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -86,5 +97,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
