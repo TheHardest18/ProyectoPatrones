d005c90fd3ca28bc639d1094bd05dd151e7b9db8
TheHardest18 <isias1626@gmail.com>
2020-08-06 17:12:20 -0400

    Final

--- ProyectoFinal2/ProyectoFinal2/Learn.Designer.cs
+++ ProyectoFinal2/ProyectoFinal2/Learn.Designer.cs
@@ -28,22 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Learn));
+            this.imgFotos = new System.Windows.Forms.ImageList(this.components);
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.imgFotos2 = new System.Windows.Forms.ImageList(this.components);
+            this.imgFotos3 = new System.Windows.Forms.ImageList(this.components);
+            this.img4 = new System.Windows.Forms.ImageList(this.components);
+            this.btnReproducir1 = new System.Windows.Forms.Button();
+            this.listimg1 = new System.Windows.Forms.Label();
+            this.listimg4 = new System.Windows.Forms.Label();
+            this.listimg3 = new System.Windows.Forms.Label();
+            this.listimg2 = new System.Windows.Forms.Label();
+            this.btnReproducir2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnModulos = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // imgFotos
+            // 
+            this.imgFotos.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgFotos.ImageStream")));
+            this.imgFotos.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgFotos.Images.SetKeyName(0, "cola.JPG");
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(324, 360);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(82, 34);
+            this.btnSiguiente.TabIndex = 5;
+            this.btnSiguiente.Text = "Next";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // imgFotos2
+            // 
+            this.imgFotos2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgFotos2.ImageStream")));
+            this.imgFotos2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgFotos2.Images.SetKeyName(0, "cola.JPG");
+            this.imgFotos2.Images.SetKeyName(1, "cola2.jpg");
+            // 
+            // imgFotos3
+            // 
+            this.imgFotos3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgFotos3.ImageStream")));
+            this.imgFotos3.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgFotos3.Images.SetKeyName(0, "water.JPG");
+            // 
+            // img4
+            // 
+            this.img4.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("img4.ImageStream")));
+            this.img4.TransparentColor = System.Drawing.Color.Transparent;
+            this.img4.Images.SetKeyName(0, "water2.jpg");
+            // 
+            // btnReproducir1
+            // 
+            this.btnReproducir1.Image = ((System.Drawing.Image)(resources.GetObject("btnReproducir1.Image")));
+            this.btnReproducir1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReproducir1.Location = new System.Drawing.Point(183, 53);
+            this.btnReproducir1.Name = "btnReproducir1";
+            this.btnReproducir1.Size = new System.Drawing.Size(78, 75);
+            this.btnReproducir1.TabIndex = 9;
+            this.btnReproducir1.UseVisualStyleBackColor = true;
+            this.btnReproducir1.Click += new System.EventHandler(this.btnReproducir1_Click);
+            // 
+            // listimg1
+            // 
+            this.listimg1.ImageIndex = 0;
+            this.listimg1.ImageList = this.imgFotos;
+            this.listimg1.Location = new System.Drawing.Point(46, 9);
+            this.listimg1.Name = "listimg1";
+            this.listimg1.Size = new System.Drawing.Size(90, 154);
+            this.listimg1.TabIndex = 8;
+            // 
+            // listimg4
+            // 
+            this.listimg4.ImageIndex = 0;
+            this.listimg4.ImageList = this.img4;
+            this.listimg4.Location = new System.Drawing.Point(309, 176);
+            this.listimg4.Name = "listimg4";
+            this.listimg4.Size = new System.Drawing.Size(90, 154);
+            this.listimg4.TabIndex = 7;
+            // 
+            // listimg3
+            // 
+            this.listimg3.ImageIndex = 0;
+            this.listimg3.ImageList = this.imgFotos3;
+            this.listimg3.Location = new System.Drawing.Point(46, 176);
+            this.listimg3.Name = "listimg3";
+            this.listimg3.Size = new System.Drawing.Size(90, 154);
+            this.listimg3.TabIndex = 6;
+            // 
+            // listimg2
+            // 
+            this.listimg2.ImageIndex = 1;
+            this.listimg2.ImageList = this.imgFotos2;
+            this.listimg2.Location = new System.Drawing.Point(309, 9);
+            this.listimg2.Name = "listimg2";
+            this.listimg2.Size = new System.Drawing.Size(90, 154);
+            this.listimg2.TabIndex = 1;
+            this.listimg2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnReproducir2
+            // 
+            this.btnReproducir2.Image = ((System.Drawing.Image)(resources.GetObject("btnReproducir2.Image")));
+            this.btnReproducir2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReproducir2.Location = new System.Drawing.Point(183, 217);
+            this.btnReproducir2.Name = "btnReproducir2";
+            this.btnReproducir2.Size = new System.Drawing.Size(78, 75);
+            this.btnReproducir2.TabIndex = 10;
+            this.btnReproducir2.UseVisualStyleBackColor = true;
+            this.btnReproducir2.Click += new System.EventHandler(this.btnReproducir2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(186, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Listen";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 32);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Listen";
+            // 
+            // button1
+            // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(183, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 75);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(183, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 75);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnModulos
+            // 
+            this.btnModulos.Location = new System.Drawing.Point(183, 360);
+            this.btnModulos.Name = "btnModulos";
+            this.btnModulos.Size = new System.Drawing.Size(82, 34);
+            this.btnModulos.TabIndex = 13;
+            this.btnModulos.Text = "Modules";
+            this.btnModulos.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(54, 360);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(82, 34);
+            this.btnBack.TabIndex = 14;
+            this.btnBack.Text = "Previous";
+            this.btnBack.UseVisualStyleBackColor = true;
             // 
             // Learn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 429);
+            this.ClientSize = new System.Drawing.Size(457, 448);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnModulos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReproducir2);
+            this.Controls.Add(this.btnReproducir1);
+            this.Controls.Add(this.listimg1);
+            this.Controls.Add(this.listimg4);
+            this.Controls.Add(this.listimg3);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.listimg2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Learn";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Learn";
+            this.Load += new System.EventHandler(this.Learn_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imgFotos;
+        private System.Windows.Forms.Label listimg2;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.ImageList imgFotos2;
+        private System.Windows.Forms.ImageList imgFotos3;
+        private System.Windows.Forms.ImageList img4;
+        private System.Windows.Forms.Label listimg3;
+        private System.Windows.Forms.Label listimg4;
+        private System.Windows.Forms.Label listimg1;
+        private System.Windows.Forms.Button btnReproducir1;
+        private System.Windows.Forms.Button btnReproducir2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnModulos;
+        private System.Windows.Forms.Button btnBack;
     }
 }
--- ProyectoFinal2/ProyectoFinal2/Learn.cs
+++ ProyectoFinal2/ProyectoFinal2/Learn.cs
@@ -7,14 +7,244 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+using WMPLib;
 
 namespace ProyectoFinal2
 {
     public partial class Learn : Form
     {
+        private int indice;
+        private int indice1;
+        private int indice2;
+        private int indice3;
         public Learn()
         {
             InitializeComponent();
+            indice = 0;
+        }
+
+        private void Learn_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            indice++;
+            indice1++;
+            indice2++;
+            indice3++;
+            if (indice > 10) 
+            {
+                indice = 0;
+                
+            }
+            if (indice1 > 10) 
+            {
+                indice1 = 0;
+                
+            } 
+            if (indice2 > 10) 
+            {
+                indice2 = 0;
+                
+            }  
+            if (indice3 > 10) 
+            {
+                indice3 = 0;
+                
+            }
+            listimg1.ImageIndex = indice;
+            listimg2.ImageIndex = indice1;
+            listimg3.ImageIndex = indice2;
+            listimg4.ImageIndex = indice3;
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        //SoundPlayer sonido;//wav
+        WindowsMediaPlayer sonido;//mp3
+        private void audio1() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }private void audio2() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/water1596665513.mp3";
+            sonido.controls.play();
+            
+        }private void audio3() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }private void audio4() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }private void audio5() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }private void audio6() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }private void audio7() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }private void audio8() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }private void audio9() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }
+        private void audio10() 
+        {
+            sonido = new WindowsMediaPlayer();
+            sonido.URL = @"https://storage.googleapis.com/nfree/cola1596658003.mp3";
+            sonido.controls.play();
+        }
+        private void btnReproducir1_Click(object sender, EventArgs e)
+        {
+            try {
+                if (sonido == null && indice==0) 
+                {
+                    audio1();
+
+                }
+
+           // else if (sonido != null && indice == 3)
+           // {
+           //     audio4();
+
+           // }
+           // else if (sonido != null && indice == 4)
+           // {
+           //     audio5();
+
+           // }
+           // else if (sonido != null && indice == 5)
+           // {
+           //     audio6();
+
+           // }
+           // else if (sonido != null && indice == 6)
+           // {
+           //     audio7();
+
+           // }
+           // else if (sonido != null && indice == 7)
+           // {
+           //     audio8();
+
+           // }
+           //else if (sonido != null && indice == 8)
+           // {
+           //     audio9();
+
+           // }
+           //else if (sonido != null && indice == 9)
+           // {
+           //     audio10();
+
+         //   }
+            else if (sonido != null )
+                {
+                    sonido.controls.stop();
+                    sonido = null;
+
+                }
+
+            }
+            catch (Exception error) 
+            {
+                MessageBox.Show("Error al reproducir el audio"+error);
+            }
+        }
+
+        private void btnReproducir2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sonido != null && indice1 == 0)
+                {
+                    audio2();
+                }
+
+                else if(sonido == null && indice1 == 0) 
+                {
+                    audio2();
+                }
+              
+                //else if (sonido != null && indice == 2)
+                //{
+                //    audio3();
+
+                //}
+                //else if (sonido != null && indice == 3)
+                //{
+                //    audio4();
+
+                //}
+                //else if (sonido != null && indice == 4)
+                //{
+                //    audio5();
+
+                //}
+                //else if (sonido != null && indice == 5)
+                //{
+                //    audio6();
+
+                //}
+                //else if (sonido != null && indice == 6)
+                //{
+                //    audio7();
+
+                //}
+                //else if (sonido != null && indice == 7)
+                //{
+                //    audio8();
+
+                //}
+                //else if (sonido != null && indice == 8)
+                //{
+                //    audio9();
+
+                //}
+                //else if (sonido != null && indice == 9)
+                //{
+                //    audio10();
+
+                //}
+
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error al reproducir el audio" + error);
+            }
         }
     }
 }
