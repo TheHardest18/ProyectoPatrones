ee234e6edc29508103d70b8b49b6fe64133236e3
IsraelPeniel <penielpmd@users.noreply.github.com>
2020-08-06 22:53:08 -0400

    ok

--- ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
+++ ProyectoFinal2/ProyectoFinal2/Write.Designer.cs
@@ -28,37 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Write));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 228);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "mobile.JPG");
+            this.imageList1.Images.SetKeyName(1, "mouse.JPG");
+            this.imageList1.Images.SetKeyName(2, "headphones.JPG");
+            this.imageList1.Images.SetKeyName(3, "keyboard.JPG");
+            // 
+            // label1
+            // 
+            this.label1.ImageIndex = 0;
+            this.label1.ImageList = this.imageList1;
+            this.label1.Location = new System.Drawing.Point(107, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 200);
+            this.label1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(76, 297);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(358, 495);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 102);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "este soy yo";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Next";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 495);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Modulos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.ImageIndex = 0;
+            this.label2.ImageList = this.imageList2;
+            this.label2.Location = new System.Drawing.Point(107, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 200);
+            this.label2.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 482);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(197, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "bateries.JPG");
+            this.imageList2.Images.SetKeyName(1, "computer.JPG");
+            this.imageList2.Images.SetKeyName(2, "tablet.JPG");
+            this.imageList2.Images.SetKeyName(3, "water.JPG");
             // 
             // Write
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 431);
+            this.ClientSize = new System.Drawing.Size(445, 530);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Write";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Write";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- ProyectoFinal2/ProyectoFinal2/Write.cs
+++ ProyectoFinal2/ProyectoFinal2/Write.cs
@@ -12,12 +12,30 @@ namespace ProyectoFinal2
 {
     public partial class Write : Form
     {
+        private int indice;
+   
         public Write()
         {
             InitializeComponent();
+            indice = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            indice++;
+            if (indice > 4)
+                indice = 0;
+
+            label1.ImageIndex = indice;
+            label2.ImageIndex = indice;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
