54e8a8fbc0f4a7cc8f4ba1292deaf4cae2c22526
TheHardest18 <isias1626@gmail.com>
2020-08-07 20:27:15 -0400

    conexiom

--- ProyectoFinal2/ProyectoFinal2/Form1.cs
+++ ProyectoFinal2/ProyectoFinal2/Form1.cs
@@ -19,8 +19,8 @@ namespace ProyectoFinal2
     {
         IFirebaseConfig config = new FirebaseConfig
         {
-            AuthSecret = "PLVkEMQDn0cXiTp6JtG6aU3RedKhVu08RhUldYtT",
-            BasePath = "https://proyectoprog2-41156.firebaseio.com/"
+            //AuthSecret = "PLVkEMQDn0cXiTp6JtG6aU3RedKhVu08RhUldYtT",
+            //BasePath = "https://proyectoprog2-41156.firebaseio.com/"
 
 
         };
@@ -39,7 +39,7 @@ namespace ProyectoFinal2
         {
 
         }
-
+        //
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -50,11 +50,11 @@ namespace ProyectoFinal2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            client = new FireSharp.FirebaseClient(config);
-            if(client != null) 
-            {
-                MessageBox.Show("Conexion estable");
-            }
+            //client = new FireSharp.FirebaseClient(config);
+            //if(client != null) 
+            //{
+            //    MessageBox.Show("Conexion estable");
+            //}
         }
     }
 }
