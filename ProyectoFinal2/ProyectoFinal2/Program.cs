7787810265cc26790345b34632507908c92cbe24
TheHardest18 <isias1626@gmail.com>
2020-08-11 12:03:25 -0400

    pogram

--- ProyectoFinal2/ProyectoFinal2/Program.cs
+++ ProyectoFinal2/ProyectoFinal2/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoFinal2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Form1());
         }
     }
 }
